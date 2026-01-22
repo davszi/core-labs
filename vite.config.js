9383384b8313f7d9012f31a4e378f4e1f04be6c4
Dávid Szilágyi <95181353+davszi@users.noreply.github.com>
2026-01-22 10:37:07 +0200

    Change base path from '/core/' to '/core-labs/'

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/core/',
+  base: '/core-labs/',
   assetsInclude: ['**/*.mp4', '**/*.MP4', '**/*.webm', '**/*.gif'],
   build: {
     rollupOptions: {
